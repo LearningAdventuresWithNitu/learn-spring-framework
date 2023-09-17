01b5b0c267cbbaa6033d9a094782ea3f8ca0cc9b
LearningAdventuresWithNitu <142468489+LearningAdventuresWithNitu@users.noreply.github.com>
2023-09-17 09:35:22 -0400

    Iteration3: Spring Context & Config, Bean creation

--- src/main/java/com/learningadventures/learnspringframework/App01GamingBasic.java
+++ src/main/java/com/learningadventures/learnspringframework/App01GamingBasic.java
@@ -6,13 +6,16 @@ import com.learningadventures.learnspringframework.game.MarioGame;
 import com.learningadventures.learnspringframework.game.PacmanGame;
 import com.learningadventures.learnspringframework.game.SuperContraGame;
 
-public class AppGamingBasic {
+public class App01GamingBasic {
     public static void main(String[] args) {
         // GamingConsole game = new PacmanGame();
         // GamingConsole game = new SuperContraGame();
-        GamingConsole game = new MarioGame();
+        GamingConsole game = new MarioGame(); // Here, we are creating the object but now we want the Spring Framework to do that for us and wire them together.
         
-        GameRunner gameRunner = new GameRunner(game);
+        GameRunner gameRunner = new GameRunner(game); // Object Creation + Wiring of Dependencies
+        // Since GameRunner class needs a game object to run so we can say that game is a dependency of the GameRunner class.
+        // We see that we are injecting the dependency game in the GameRunner class when passing it to the constructor in line 15.
+                                                    
         gameRunner.run();        
     }
     
